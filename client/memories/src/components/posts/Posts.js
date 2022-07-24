7d759c9ffbc4d5326356af7a0763d95b8d810a36
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-07-24 10:26:10 +0100

    working on the front end

---
+++ client/memories/src/components/posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./post/post";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
+export default Posts;
---
+++ client/memories/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  heading: {
+    color: "rgba(0,183,255, 1)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
