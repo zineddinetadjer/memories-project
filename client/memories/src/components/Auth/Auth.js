1c6e2850173361b3386f23199cc166b17113205b
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-08-26 17:31:57 +0100

    adding the google auth and working on the sign in form

--- client/memories/src/components/Auth/Auth.js
+++ client/memories/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
@@ -7,14 +7,127 @@ import {
   Typography,
   Container,
 } from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
+import Input from "./Input";
+import Icon from "./Icon";
 
 function Auth() {
   const classes = useStyles();
-  const state = null;
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const swicthMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+
+  const googleSuccess = async (res) => {
+    console.log(res);
+  };
+  const googleFailure = () => {
+    console.log("Google sign in was unseccessful");
+  };
+
   return (
     <div>
-      <Container component="main" maxWidth="xs"></Container>
+      <Container component="main" maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              {isSignup && (
+                <>
+                  <Input
+                    name="first name"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                    half
+                  />
+                  <Input
+                    name="first name"
+                    label="Last Name"
+                    handleChange={handleChange}
+                    half
+                  />
+                </>
+              )}
+              <Input
+                name="email"
+                label="Email Adress"
+                handleChange={handleChange}
+                type="email"
+              />
+              <Input
+                name="password"
+                label="Password"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+              {isSignup && (
+                <Input
+                  name="confirmPassword"
+                  label="Confirm Your Password"
+                  handleChange={handleChange}
+                  type="password"
+                />
+              )}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {isSignup ? "Sign Up" : "Sign In"}
+            </Button>
+            <GoogleLogin
+              clientId="753864762333-lvn3hlpajhphjbcf9q0nlq8hpdceqsrl.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <Button
+                  className={classes.googleButton}
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  startIcon={<Icon />}
+                  variant="contained"
+                >
+                  Sign in with Google
+                </Button>
+              )}
+              onSuccess={googleSuccess}
+              onFailure={googleFailure}
+              cookiePolicy="single_host_origin"
+            />
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Button onClick={swicthMode}>
+                  {isSignup
+                    ? "Already have an account? Sign In"
+                    : "Don't have a account Sign Up "}
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Container>
     </div>
   );
 }
