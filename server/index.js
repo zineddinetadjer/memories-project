a31f76b80e4667546e6dd14e0149b520fbc76341
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-10-21 14:13:27 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
-import userRoutes from "./routes/users.js";
+import userRouter from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-app.use("/user", userRoutes);
+app.use("/user", userRouter);
 
 //const CONNECTION_URL ="mongodb+srv://zineddine:zinou17@cluster01.cxsva.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
